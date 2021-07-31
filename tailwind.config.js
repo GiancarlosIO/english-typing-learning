e76b313f31ab2005c4e5309f06b3b3abfbe3c6c4
Giancarlos Isasi <giancarlos@crehana.com>
2021-07-31 14:01:51 -0500

    configure tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
